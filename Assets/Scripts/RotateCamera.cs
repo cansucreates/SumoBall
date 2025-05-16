9c4104d7be0f5a217d764bd2c68a61c346b7d46f
cansucreates <caysagdic@gmail.com>
2025-05-17 00:08:53 +0300

    Added camera rotation

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        
+    }
+}
