a5ab86063af8f5acc284bb1178c0ec897bd315e3
cansucreates <caysagdic@gmail.com>
2025-05-18 00:16:38 +0300

    Added new wave of enemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,14 +4,26 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab; // Reference to the enemy prefab to spawn
     private float spawnRange = 9;
+    public int enemyCount; // Number of enemies to spawn
+    public int waveNumber = 1; // Current wave number
+    public GameObject powerupPrefab; // Reference to the powerup prefab to spawn
 
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
-    }
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); // Spawn a powerup at the start
+     }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length; // Count the number of active enemies in the scene
+        if (enemyCount == 0)
+        {
+            waveNumber++; // Increment the wave number
+            SpawnEnemyWave(waveNumber); // If no enemies are present, spawn a new wave
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); // Spawn a new powerup
+        }
+    }
 
     private Vector3 GenerateSpawnPosition()
     {
@@ -19,5 +31,13 @@ public class SpawnManager : MonoBehaviour
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
